4c7d52c08a51b9b21914095c5a58a334d87ba274
fedi96 <fedi96@users.noreply.github.com>
2017-11-16 08:28:46 -0800

    Create xhr.js

---
+++ xhr.js
@@ -0,0 +1,2 @@
+(function(open){XMLHttpRequest.prototype.open=function(method,url,async,user,pass){if(url.indexOf('mlb-ws')!=-1){rewrittenUrl=url.replace("https://mlb-ws-mf.media.mlb.com","https://rjh217.herokuapp.com/http://key.rjh.fun/");}else if(url.indexOf('svc.nhl')!=-1){rewrittenUrl=url.replace("https://mf.svc.nhl.com","https://rjh217.herokuapp.com/http://key.rjh.fun/");}else if(url.indexOf('url=')!=-1){rewrittenUrl=url.replace("http://key.rjh.fun/ha.m3u8?url=","");}else{rewrittenUrl=url;}
+open.call(this,method,rewrittenUrl,async,user,pass);};})(XMLHttpRequest.prototype.open);
