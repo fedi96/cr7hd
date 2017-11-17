87870b6faff9ba4c83a5a939918cd2ece6a0cb29
fedi96 <fedi96@users.noreply.github.com>
2017-11-17 18:32:01 +0100

    Update nfl.js

--- nfl.js
+++ nfl.js
@@ -14,22 +14,10 @@ function getNFLGames() {
 				var home = game.gameSchedule.homeNickname;
 				var logoaway = game.gameSchedule.visitorTeamAbbr;
 				var logohome = game.gameSchedule.homeTeamAbbr;
-switch (logoaway) {
-    default: 
-        break;
-    case 'WAS':
-        logoaway = "wsh";
-        break;
-}
-switch (logohome) {
-    default: 
-        break;
-    case 'WAS':
-        logohome = "wsh";
-        break; 
-}
-var txc=.4+.07*i;
-var gameTitle = '<div class="box-animate animated fadeInUp" style="animation-delay: '+txc+'s;"><div class="box" style="color: rgb(254, 171, 58);background: rgb(2, 25, 38);text-align: center;"><div class="title-wrapper"><div class="title">' + away + " at " + home + '</div></div><hr><div class="button-wrapper animated fadeIn"><img style="float: left;" src="http://a1.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/scoreboard/'+ logoaway + '.png&amp;h=55&amp;w=55"><div class="xs hint--rounded hint--bounce" style="margin-top: 20px;"><button class="bttn-material-flat bttn-xs bttn-warning bttn-no-outline">'+ time + '</button></div><img style="float: right;" src="http://a1.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/scoreboard/' + logohome + '.png&amp;h=55&amp;w=55"></div>';
+				switch (logoaway) {default: break;case 'WAS':logoaway = "wsh";break;}
+				switch (logohome) {default: break;case 'WAS':logohome = "wsh";break;}
+				var txc=.4+.07*i;
+				var gameTitle = '<div class="box-animate animated fadeInUp" style="animation-delay: '+txc+'s;"><div class="box" style="color: rgb(254, 171, 58);background: rgb(2, 25, 38);text-align: center;"><div class="title-wrapper"><div class="title">' + away + " at " + home + '</div></div><hr><div class="button-wrapper animated fadeIn"><img style="float: left;" src="http://a1.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/scoreboard/'+ logoaway + '.png&amp;h=55&amp;w=55"><div class="xs hint--rounded hint--bounce" style="margin-top: 20px;"><button class="bttn-material-flat bttn-xs bttn-warning bttn-no-outline">'+ time + '</button></div><img style="float: right;" src="http://a1.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/scoreboard/' + logohome + '.png&amp;h=55&amp;w=55"></div>';
 				var gameLinks = "";
 				gameLinks = gameLinks + "<a href='http://rjh.fun/play.html?league=NFL&pk="+pk+"' data-ajax='false'><button class='bttn-slant bttn-xs bttn-warning'>HOME</button></a>";
 				gameLinks = gameLinks + "</div></div>";
@@ -41,7 +29,7 @@ var gameTitle = '<div class="box-animate animated fadeInUp" style="animation-del
 	});
 }
 
-function getNHLGameInfo(cdn, pk, id, date) {
+function getNFLGameInfo(cdn, pk, id, date) {
 	var nhlAPI = "https://statsapi.web.nhl.com/api/v1/schedule?gamePk=" + pk + "&expand=schedule.teams,schedule.linescore,schedule.broadcasts,schedule.ticket,schedule.game.content.media.epg&leaderCategories=&site=en_nhl&teamId=";
 	var gameTitle;
 	$.getJSON(nhlAPI, function (json) {
