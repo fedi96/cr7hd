7e64ea3d2a04c0c234034b889548c589b2353f77
fedi96 <fedi96@users.noreply.github.com>
2017-11-23 08:39:17 +0100

    Update xhr.js

--- xhr.js
+++ xhr.js
@@ -2,9 +2,24 @@
   if(url.indexOf('mlb-ws')!=-1){rewrittenUrl=url.replace("https://mlb-ws-mf.media.mlb.com","https://eplsitea.herokuapp.com/http://key.rjh.fun");}
   else if(url.indexOf('svc.nhl')!=-1){rewrittenUrl=url.replace("https://mf.svc.nhl.com","https://eplsitea.herokuapp.com/http://key.rjh.fun");}
   else if(url.indexOf('url=')!=-1){rewrittenUrl=url.replace("http://rjh217.stream/m3u8.m3u8?url=","");}
-  else if(url.indexOf("m3u8.key") !== -1){rewrittenUrl= "https://eplsitea.herokuapp.com/http://cros.mybacklink.website/k2.php?url="+ btoa(url);}
   else{rewrittenUrl=url;}
 open.call(this,method,rewrittenUrl,async,user,pass);};})(XMLHttpRequest.prototype.open);
 
 
-
+var replacekey = ["hls/securekey","op-generatekey/v-2.3","2.3/key/silk/","/key-service/mlbnkid"];
+            (function (open) {
+                XMLHttpRequest.prototype.open = function (method, url, async, user, pass) {
+                    isKeyUrl = false;
+                    $.each(replacekey, function (key, value) {
+                        if (url.toLowerCase().indexOf(value) >= 0) {
+                            isKeyUrl = true;
+                        }
+                    });
+                    if (isKeyUrl) {
+                        rewrittenUrl = "https://eplsitea.herokuapp.com/http://cros.mybacklink.website/k2.php?url="+ btoa(url);
+                    } else {
+                        rewrittenUrl = url;
+                    }
+                    open.call(this, method, rewrittenUrl, async, user, pass);
+                };
+            })(XMLHttpRequest.prototype.open);
