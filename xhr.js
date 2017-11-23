f58d664cdc174978a58430b27fb056d14c035271
fedi96 <fedi96@users.noreply.github.com>
2017-11-23 08:36:06 +0100

    Update xhr.js

--- xhr.js
+++ xhr.js
@@ -2,7 +2,7 @@
   if(url.indexOf('mlb-ws')!=-1){rewrittenUrl=url.replace("https://mlb-ws-mf.media.mlb.com","https://eplsitea.herokuapp.com/http://key.rjh.fun");}
   else if(url.indexOf('svc.nhl')!=-1){rewrittenUrl=url.replace("https://mf.svc.nhl.com","https://eplsitea.herokuapp.com/http://key.rjh.fun");}
   else if(url.indexOf('url=')!=-1){rewrittenUrl=url.replace("http://rjh217.stream/m3u8.m3u8?url=","");}
-  else if(url.indexOf('hls/securekey')>0){rewrittenUrl=url.replace(url,"https://eplsitea.herokuapp.com/http://cros.mybacklink.website/k2.php?url=nba");}
+  else if(url.indexOf("m3u8.key") !== -1){rewrittenUrl= "https://eplsitea.herokuapp.com/http://cros.mybacklink.website/k2.php?url="+ btoa(url);}
   else{rewrittenUrl=url;}
 open.call(this,method,rewrittenUrl,async,user,pass);};})(XMLHttpRequest.prototype.open);
 
