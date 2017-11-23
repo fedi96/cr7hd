376b7d90e75ed9f5221d8a9cf51097159aa633db
fedi96 <fedi96@users.noreply.github.com>
2017-11-23 14:54:21 +0100

    Update xhr.js

--- xhr.js
+++ xhr.js
@@ -1,13 +1,3 @@
-(function (open) {
-    XMLHttpRequest.prototype.open = function (method, url, async, user, pass) {
-  if (url.toLowerCase().indexOf("fwmrm.net") >= 0) {
-   return;
-  }
-  var rewrittenUrl = url;
-    rewrittenUrl=url.replace("http://nlsk.neulion.com/nlsk1/hls/securekey?id=21&url=","https://eplsitea.herokuapp.com/http://rjh217.stream/keynfl.php?url=");
-    open.call(this, method, rewrittenUrl, async, user, pass);
-    };
-})(XMLHttpRequest.prototype.open);
 (function(open){XMLHttpRequest.prototype.open=function(method,url,async,user,pass){
   if(url.indexOf('mlb-ws')!=-1){rewrittenUrl=url.replace("https://mlb-ws-mf.media.mlb.com","https://eplsitea.herokuapp.com/http://key.rjh.fun");}
   else if(url.indexOf('svc.nhl')!=-1){rewrittenUrl=url.replace("https://mf.svc.nhl.com","https://eplsitea.herokuapp.com/http://key.rjh.fun");}
@@ -17,6 +7,8 @@ open.call(this,method,rewrittenUrl,async,user,pass);};})(XMLHttpRequest.prototyp
 var replacekey = ["hls/securekey","op-generatekey/v-2.3"];
 (function (open) {XMLHttpRequest.prototype.open = function (method, url, async, user, pass) {isKeyUrl = false;$.each(replacekey, function (key, value) {
 if (url.toLowerCase().indexOf(value) >= 0) {isKeyUrl = true;}});
-if (isKeyUrl) {rewrittenUrl = "https://eplsitea.herokuapp.com/http://rjh217.stream/k2.php?url="+ btoa(url);}
+if(url.indexOf('nlsk1/hls/securekey?id=21&url=')!=-1){    rewrittenUrl=url.replace("http://nlsk.neulion.com/nlsk1/hls/securekey?id=21&url=","https://eplsitea.herokuapp.com/http://rjh217.stream/keynfl.php?url=");
+                           }                                                                                      
+else if (isKeyUrl) {rewrittenUrl = "https://eplsitea.herokuapp.com/http://rjh217.stream/k2.php?url="+ btoa(url);}
 else {rewrittenUrl = url;}
 open.call(this, method, rewrittenUrl, async, user, pass);};})(XMLHttpRequest.prototype.open);
