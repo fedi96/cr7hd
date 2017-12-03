f9c3fb950acf07edc62ed958919111d3be280637
fedi96 <fedi96@users.noreply.github.com>
2017-12-03 21:35:52 +0100

    Update xhr.js

--- xhr.js
+++ xhr.js
@@ -8,7 +8,9 @@ var replacekey = ["hls/securekey","op-generatekey/v-2.3"];
 (function (open) {XMLHttpRequest.prototype.open = function (method, url, async, user, pass) {isKeyUrl = false;$.each(replacekey, function (key, value) {
 if (url.toLowerCase().indexOf(value) >= 0) {isKeyUrl = true;}});
 if(url.indexOf('21&url=')!=-1){    rewrittenUrl=url.replace("http://nlsk.neulion.com/nlsk1/hls/securekey?id=21&url=","http://rjh217.stream/keynfl.php?url=");
-                           }                                                                                      
+                           }     
+else if(url.indexOf('16&url=')!=-1){    rewrittenUrl=url.replace("http://nlsk.neulion.com/nlsk1/hls/securekey?","http://keys.rjh.fun/keys/nba?url=");
+                           } 
 else if (isKeyUrl) {rewrittenUrl = "/keyn.txt?url="+ btoa(url);}
 else {rewrittenUrl = url;}
 open.call(this, method, rewrittenUrl, async, user, pass);};})(XMLHttpRequest.prototype.open);
