e97fcbf26ea2bb2e6543a1e8c6588b156ae40495
fedi96 <fedi96@users.noreply.github.com>
2017-12-15 23:53:15 +0100

    Update xhr.js

--- xhr.js
+++ xhr.js
@@ -1,6 +1,6 @@
 (function(open){XMLHttpRequest.prototype.open=function(method,url,async,user,pass){
-  if(url.indexOf('mlb-ws')!=-1){rewrittenUrl=url.replace("https://mlb-ws-mf.media.mlb.com","https://jokercros.herokuapp.com/http://key.rjh.fun");}
-  else if(url.indexOf('svc.nhl')!=-1){rewrittenUrl=url.replace("https://mf.svc.nhl.com","https://jokercros.herokuapp.com/http://key.rjh.fun");}
+  if(url.indexOf('mlb-ws')!=-1){rewrittenUrl=url.replace("https://mlb-ws-mf.media.mlb.com","https://spark-cinema.glitch.me/http://key.rjh.fun");}
+  else if(url.indexOf('svc.nhl')!=-1){rewrittenUrl=url.replace("https://mf.svc.nhl.com","https://spark-cinema.glitch.me/http://key.rjh.fun");}
   else if(url.indexOf('url=')!=-1){rewrittenUrl=url.replace("http://rjh217.stream/m3u8.m3u8?url=","");}
   else{rewrittenUrl=url;}
 open.call(this,method,rewrittenUrl,async,user,pass);};})(XMLHttpRequest.prototype.open);
