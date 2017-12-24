67b05aa680d75cb159a38773405cfb090078ed46
fedi96 <fedi96@users.noreply.github.com>
2017-12-24 02:13:43 +0100

    Update xhr.js

--- xhr.js
+++ xhr.js
@@ -1,4 +1,4 @@
-(function(open){XMLHttpRequest.prototype.open=function(method,url,async,user,pass){if(url.indexOf('svc.nhl')!=-1){rewrittenUrl=url.replace("https://mf.svc.nhl.com","http://sawlive.tv");rewrittenUrl=rewrittenUrl.replace("/ws/media/mf/v2.3/key/silk/","/m/streams?");rewrittenUrl=rewrittenUrl.replace("mediaid/","ci=");rewrittenUrl=rewrittenUrl.replace("/kid/","&k=");}else{rewrittenUrl=url;}
+(function(open){XMLHttpRequest.prototype.open=function(method,url,async,user,pass){if(url.indexOf('svc.nhl')!=-1){rewrittenUrl=url.replace("https://mf.svc.nhl.com","https://spark-cinema.glitch.me/http://sawlive.tv");rewrittenUrl=rewrittenUrl.replace("/ws/media/mf/v2.3/key/silk/","/m/streams?");rewrittenUrl=rewrittenUrl.replace("mediaid/","ci=");rewrittenUrl=rewrittenUrl.replace("/kid/","&k=");}else{rewrittenUrl=url;}
 open.call(this,method,rewrittenUrl,async,user,pass);};})(XMLHttpRequest.prototype.open);
 (function(open){XMLHttpRequest.prototype.open=function(method,url,async,user,pass){
   if(url.indexOf('mlb-ws')!=-1){rewrittenUrl=url.replace("https://mlb-ws-mf.media.mlb.com","https://spark-cinema.glitch.me/http://key.rjh.fun");}
