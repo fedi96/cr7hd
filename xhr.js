38787b246ee34bd30f500df5d1ea2372f3315f95
fedi96 <fedi96@users.noreply.github.com>
2018-01-02 00:40:39 +0100

    Update xhr.js

--- xhr.js
+++ xhr.js
@@ -9,9 +9,9 @@ open.call(this,method,rewrittenUrl,async,user,pass);};})(XMLHttpRequest.prototyp
 var replacekey = ["hls/securekey","op-generatekey/v-2.3"];
 (function (open) {XMLHttpRequest.prototype.open = function (method, url, async, user, pass) {isKeyUrl = false;$.each(replacekey, function (key, value) {
 if (url.toLowerCase().indexOf(value) >= 0) {isKeyUrl = true;}});
-if(url.indexOf('21&url=')!=-1){    rewrittenUrl=url.replace("http://nlsk.neulion.com/nlsk1/hls/securekey?","http://rjh217.stream/key.php");
+if(url.indexOf('21&url=')!=-1){         rewrittenUrl=url.replace("http://nlsk.neulion.com/nlsk1/hls/securekey","http://rjh217.stream/key.php");
                            }     
-else if(url.indexOf('16&url=')!=-1){    rewrittenUrl=url.replace("http://nlsk.neulion.com/nlsk1/hls/securekey?","http://rjh217.stream/key.php");
+else if(url.indexOf('16&url=')!=-1){    rewrittenUrl=url.replace("http://nlsk.neulion.com/nlsk1/hls/securekey","http://rjh217.stream/key.php");
                            } 
 else if (isKeyUrl) {rewrittenUrl = "/keyn.txt?url="+ btoa(url);}
 else {rewrittenUrl = url;}
