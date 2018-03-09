3437cf28b4be7213f944eb7cc8fcc848cddb1314
fedi96 <fedi96@users.noreply.github.com>
2018-03-09 23:14:30 +0100

    Update main.js

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ function rjhply(id,cdn,date){
 	if(id==="HOME"){id='h'}
 	if(id==="AWAY"){id='a'}
 	if($_GET("league")==="NHL"){//url="https://spark-cinema.glitch.me/https://alive-nitrogen.glitch.me/"+cdn+"/"+id;
-		url="https://spark-cinema.glitch.me/https://key.rjh.fun/m3u8/"+date+"/"+id+cdn;
+		url="http://nba.rjh.fun/https://key.rjh.fun/m3u8/"+date+"/"+id+cdn;
 				    chatx="nhl"}
 	else if($_GET("league")==="NBA"){//url="http://rjh217.stream/nba.php?date="+date+"&cdn="+ cdn;
 		url="http://cdn.rjh.fun/"+cdn;
@@ -54,7 +54,7 @@ function rjhply(id,cdn,date){
 	if(cdn==="nfltv"){url='/nfltv'}
 	if(cdn==="redzone"){url='/redzone'}
 	$.get(url,function(data){
-		$("#player").append('<script src="//rjh.fun/vjs.js"></script><video class="video-js vjs-default-skin col my-4" poster="" width="100%" height="480" controls><source src="https://cdn.rjh.fun/'+data+'" type="application/x-mpegURL"></video><script>(function(){window.hola_player();})();<\/script>');
+		$("#player").append('<div id="plu"></div><script>var hlssrc=data;<\/script><script src="//rjh.fun/cjs.js"></script>');
 		$("#sidebar").append('<script id="cid0020000172461619766" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 100%;height: 480;">{"handle":"'+chatx+'rjhfun","arch":"js","styles":{"a":"021926","b":100,"c":"FFFFFF","d":"FFFFFF","e":"021926","g":"feab3a","h":"ffffff","k":"021926","l":"000000","m":"021926","n":"FFFFFF","p":"10","q":"021926","r":100,"t":0,"ab":false,"surl":0}}<\/script>');
 	});}
 	else{
