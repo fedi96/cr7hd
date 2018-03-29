efde31053d22a7d06ca51921337dd6fa3521bc60
fedi96 <fedi96@users.noreply.github.com>
2018-03-30 00:16:08 +0100

    Add files via upload

---
+++ streams/js/main.js
@@ -0,0 +1,54 @@
+app.controller('streams', ['$scope', function($scope) {
+$scope.stream =[
+{
+  name: 'Skysports 2',
+  value: 'Skysports 2',
+  img: 'http://mybacklink.website/images/sky.png',
+  link: 'trdt'
+},
+{
+  name: 'Skysports 2',
+  value: 'Skysports 2',
+  img: 'http://mybacklink.website/images/sky.png',
+  link: 'trdt'
+},{
+  name: 'Skysports 2',
+  value: 'Skysports 2',
+  img: 'http://mybacklink.website/images/sky.png',
+  link: 'trdt'
+},
+{
+  name: 'Skysports 2',
+  value: 'Skysports 2',
+  img: 'http://mybacklink.website/images/sky.png',
+  link: 'trdt'
+},{
+  name: 'Skysports 2',
+  value: 'Skysports 2',
+  img: 'http://mybacklink.website/images/sky.png',
+  link: 'trdt'
+},
+{
+  name: 'Skysports 2',
+  value: 'Skysports 2',
+  img: 'http://mybacklink.website/images/sky.png',
+  link: 'http://server.tn'
+},{
+  name: 'Skysports 2',
+  value: 'Skysports 2',
+  img: 'http://mybacklink.website/images/sky.png',
+  link: 'http://server.tn'
+},
+{
+  name: 'Skysports 2',
+  value: 'Skysports 2',
+  img: 'http://mybacklink.website/images/sky.png',
+  link: 'http://server.tn'
+},{
+  name: 'Skysports 2',
+  value: 'Skysports 2',
+  img: 'http://mybacklink.website/images/sky.png',
+  link: 'http://server.tn'
+}
+];
+}]);
