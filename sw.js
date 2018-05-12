f2a1c0f7b6c743c3fd0cbd604d4a8da280c057b7
fedi96 <fedi96@users.noreply.github.com>
2018-05-12 16:08:12 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,8 +2,9 @@ self.addEventListener('fetch', function(event) {
   // url ending in ".jpg"
 if(event.request.url.endsWith('teamId='))
 {
+  event.respondWith(
   new Response(response.body, {
-    headers: {'Access-Control-Allow-Origin:': 'tested'},
-});
+    headers: {'Access-Control-Allow-Origin:': 'tested'}
+}));
 }
 });
