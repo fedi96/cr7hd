32ff0b4931b33eeaced72a65b5d52bc4a3713690
fedi96 <fedi96@users.noreply.github.com>
2018-05-12 16:00:56 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,13 @@
-
-  addEventListener('fetch', event => {
-  event.respondWith(fetchAndLog(event.request))
-})
-async function fetchAndLog(request) {
-  console.log('Got request', request)
-  const response = await fetch(request)
-  console.log('Got response', response)
-  return response
+self.addEventListener('fetch', function(event) {
+  // url ending in ".jpg"
+if(event.request.url.endsWith('teamId='))
+{
+  var req = new Request(request.url, {
+    method: request.method,
+    headers: {'Origin': 'null'},
+    mode: 'same-origin', // need to set this properly
+    credentials: request.credentials,
+    redirect: 'manual'   // let browser handle redirects
+});
 }
+});
