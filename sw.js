e92c4228a008010a83df0a418e6dd4dc3bf8c2e4
fedi96 <fedi96@users.noreply.github.com>
2018-05-12 16:05:50 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,12 +2,8 @@ self.addEventListener('fetch', function(event) {
   // url ending in ".jpg"
 if(event.request.url.endsWith('teamId='))
 {
-  var req = new Request(request.url, {
-    method: request.method,
-    headers: {'Origin': 'null'},
-    mode: 'same-origin', // need to set this properly
-    credentials: request.credentials,
-    redirect: 'manual'   // let browser handle redirects
+  new Response(response.body, {
+    headers: {'Access-Control-Allow-Origin:': 'tested'},
 });
 }
 });
