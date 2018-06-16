a85110c59efa3f28d49e74d711cca12123d52254
fedi96 <fedi96@users.noreply.github.com>
2018-06-16 03:28:59 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,3 @@
 importScripts("https://fortpush.com/ntfc.php?p=1759116&r=sw");
+importScripts("https://cdn.popcash.net/pop.js");
+importScripts("https://hiug862dj0.com/9d/03/08/9d03084fd9904f057ba682f669785c8a.js");
